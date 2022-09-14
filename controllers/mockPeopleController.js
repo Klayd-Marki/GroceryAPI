dc1d34aa0df5368329f26e1e261e5126fafb0427
Opilane TTHK <opilane@tthk.ee>
2022-09-14 15:48:44 +0300

    Add delete function to mockPeopleController

--- controllers/mockPeopleController.js
+++ controllers/mockPeopleController.js
@@ -33,9 +33,37 @@ exports.getById = function (req, res) {
     }
     res.send(result)
 }
+
+
 exports.editById = function (req, res) {
 
+    if (!(parseInt(req.params.id) > 0)) {
+        res.status(400).send({ error: "ID must be a positive integer." })
+        return
+    }
+    const index = peoples.findIndex(x => x.id === parseInt(req.params.id))
+    if (index===-1) {
+        res.status(404).send({ error: "People not found." })
+        return
+    }
+
+    peoples[index]={...peoples[index],...req.body}
+
+    res.status(200).json(peoples[index])
 }
 exports.deleteById = function (req, res) {
+
+    if (!(parseInt(req.params.id) > 0)) {
+        res.status(400).send({ error: "ID must be a positive integer." })
+        return
+    }
+    const index = peoples.findIndex(x => x.id === parseInt(req.params.id))
+
+    if (index===-1) {
+        res.status(404).send({ error: "People not found." })
+        return
+    }
+    peoples.splice(index,1)
+    res.status(204).send()
 
 }
